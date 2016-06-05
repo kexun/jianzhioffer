de9fc3eb14296715aba89d16e3e857386904662f
kexun <290464784@qq.com>
2016-06-05 20:15:18 +0800

    最小的k个数字  大根堆排序方法

--- src/com/offer/Day21.java
+++ src/com/offer/Day21.java
@@ -9,11 +9,12 @@ public class Day21 {
 
 	public static void main(String[] args) {
 		int[] array = {
-			5,7,3,0,8,2,3,8,6,1,4,8
+			5,7,3,0,8,2,3,8,6,1,4,8,56,3,0,8,9
 		};
 		
 		Day21 d = new Day21();
-		d.method1(array, 3);
+//		d.method1(array, 3);
+		d.method2(array, 7);
 	}
 	
 	/**
@@ -93,5 +94,135 @@ public class Day21 {
 		}
 	}
 	
+	///////////////////////////方法二//////////////////////////////////////
+	
+	/**
+	 * 这个时间复杂度比第一种慢，但是比较时候大数据的情况下使用。
+	 * 具体做法是遍历数组，把每个元素的值和大根堆比较，当值小于大根堆的最大值，
+	 * 就放如堆中。最后得到的堆就是数组前k个小的值
+	 * @param array
+	 * @param k
+	 */
+	public void method2(int[] array, int k) {
+
+		int length = array.length;
+		if (length == 0 || k > length) {
+			return;
+		}
+		
+		Heap heap = new Heap(k);
+		
+		for (int a : array) {
+			heap.add(a);
+		}
+		
+		heap.print();
+	}
+	
+	/**
+	 * 简单实现一个大根堆，辅助排序。
+	 * 遍历数组的时候，把最小的前k个数放入堆中。
+	 * @author kexun
+	 *
+	 */
+	static class Heap {
+		
+		/**
+		 * 节点
+		 * @author kexun
+		 *
+		 */
+		
+		int size = 0;
+		int capacity;
+		
+		int[] array;
+		
+		public Heap(int capacity) {
+			this.capacity = capacity;
+			array = new int[capacity];
+		}
+		
+		public int size() {
+			return size;
+		}
+		
+		public void size(int size) {
+			this.size = size;
+		}
+		
+		public boolean isFull() {
+			return size >= capacity;
+		}
+		
+		/**
+		 * 增加一个元素
+		 * @param data
+		 */
+		public void add(int data) {
+			
+			if (isFull()) {
+				// 当堆满的时候，插入小于最大根的值，并把最大跟删除，然后从新调整堆
+				if (data < array[0]) {
+					array[0] = data;
+					heapSort();
+				}
+			} else {
+				// 堆未满的时候，直接插入值,并调整堆顺序
+				array[size] = data;
+				size++;
+				heapSort();
+			}
+		}
+		
+		public void print() {
+			for (int a : array) {
+				System.out.print(a + ",");
+			}
+		}
+		
+//		public static void main(String[] args) {
+//			Heap h = new Heap(8);
+//			h.size(8);
+//			h.heapSort();
+//			h.print();
+//			
+//		}
+		
+		/**
+		 * 堆排序
+		 */
+		public void heapSort() {
+
+			for (int i = size / 2; i >= 0; i--) {
+				heapAdjust(array, i, size - 1);
+			}
+
+		}
+		
+		public void heapAdjust(int[] arr, int s, int m) {
+
+			int temp, j;
+			temp = arr[s]; 
+
+			// 这里需要解释一下， 第一步把当前根节点排序，排完以后，对调以后的一棵树需要重新排序。
+			for (j = 2 * s+1; j <= m; j = j*2+1) {
+
+				if (j < m && arr[j] < arr[j + 1]) {
+					j++;
+				}
+
+				if (temp >= arr[j]) {
+					break;
+				}
+
+				arr[s] = arr[j];
+				s = j;
+			}
+
+			arr[s] = temp;
+		}
+		
+	}
 
 }
